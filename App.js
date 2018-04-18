8b68a47dec7fa0c9c9808b88fba5a32e9dc101b7
naichilab <naichilab@live.jp>
2018-04-19 01:51:17 +0900

    TextInput

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, View} from 'react-native';
+import {AppRegistry, Text, TextInput, View} from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
+export default class PizzaTranslator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
     render() {
         return (
-            <View style={{flex: 1}}>
-                <View style={{flex: 1, backgroundColor: 'powderblue'}}></View>
-                <View style={{flex: 2, backgroundColor: 'skyblue'}}/>
-                <View style={{flex: 3, backgroundColor: 'steelblue'}}/>
+            <View style={{padding: 10}}>
+                <TextInput style={{height: 40}} placeholder="Type here" onChangeText={(text) => this.setState({text})}/>
+                <Text style={{padding: 10, fontSize: 42}}>
+                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+                </Text>
             </View>
         );
     }
