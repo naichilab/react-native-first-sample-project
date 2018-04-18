fae191f5fa4086d54bc3fcaa30525dc3cf3afdc5
naichilab <naichilab@live.jp>
2018-04-19 02:03:13 +0900

    button layout

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, TextInput, View, Button, Alert} from 'react-native';
+import {Alert, AppRegistry, Button, StyleSheet, View} from 'react-native';
 
-export default class ButtonDemo extends Component {
+export default class ButtonBasics extends Component {
+    _onPressButton() {
+        Alert.alert('You tapped the button!');
+    }
+
     render() {
         return (
-            <View style={{padding: 10}}>
-                <Button
-                    color="green"
-                    onPress={() => {
-                        Alert.alert('You tapped the button!');
-                    }}
-                    title="Press Me"
-                />
-
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press Me"
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press Me"
+                        color="#841584"
+                    />
+                </View>
+                <View style={styles.alternativeLayoutButtonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="This looks great!"
+                    />
+                    <Button
+                        onPress={this._onPressButton}
+                        title="OK!"
+                        color="#841584"
+                    />
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        margin: 20
+    },
+    alternativeLayoutButtonContainer: {
+        margin: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+})
