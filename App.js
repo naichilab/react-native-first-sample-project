c2b547f3db8603cbb94d1d3f0bfa62d7afbb78c0
naichilab <naichilab@live.jp>
2018-04-19 01:56:04 +0900

    button

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, TextInput, View} from 'react-native';
+import {AppRegistry, Text, TextInput, View, Button, Alert} from 'react-native';
 
-export default class PizzaTranslator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {text: ''};
-    }
-
+export default class ButtonDemo extends Component {
     render() {
         return (
             <View style={{padding: 10}}>
-                <TextInput style={{height: 40}} placeholder="Type here" onChangeText={(text) => this.setState({text})}/>
-                <Text style={{padding: 10, fontSize: 42}}>
-                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-                </Text>
+                <Button
+                    color="green"
+                    onPress={() => {
+                        Alert.alert('You tapped the button!');
+                    }}
+                    title="Press Me"
+                />
+
             </View>
         );
     }
