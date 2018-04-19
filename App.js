c244485ea136731897bd3f70cb3aa907c6414971
naichilab <naichilab@live.jp>
2018-04-20 00:52:26 +0900

    format with opt + enter

--- App.js
+++ App.js
@@ -1,46 +1,43 @@
-import React, {Component} from 'react';
-import {
-    FlatList, ActivityIndicator, Text, View
-} from 'react-native';
+import React, { Component } from 'react';
+import { FlatList, ActivityIndicator, Text, View } from 'react-native';
 
 export default class FetchExample extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {isLoading: true}
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+
+  componentDidMount() {
+    return fetch('https://letsplaylist.genieus.co.jp/api/games')
+      .then(response => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.games,
+        }, () => {
+
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}><ActivityIndicator /></View>
+      );
     }
 
-    componentDidMount() {
-        return fetch('https://letsplaylist.genieus.co.jp/api/games')
-            .then((response) => response.json())
-            .then((responseJson) => {
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson.games,
-                }, function () {
-
-                });
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
-
-    render() {
-
-        if (this.state.isLoading) {
-            return (
-                <View style={{flex: 1, padding: 20}}><ActivityIndicator/></View>
-            )
-        }
-
-        return (
-            <View style={{flex: 1, paddingTop: 20}}>
-                <FlatList
-                    data={this.state.dataSource}
-                    renderItem={({item}) => <Text>{item.title}, {item.id}</Text>}
-                    keyExtractor={(item, index) => index}
-                />
-            </View>
-        )
-    }
+    return (
+      <View style={{ flex: 1, paddingTop: 20 }}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => <Text>{item.title}, {item.id}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    );
+  }
 }
